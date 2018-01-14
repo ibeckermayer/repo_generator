29f94e00abc4cf9b0ca55fcf0a33fe403bf50ea5
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-14 22:10:57 +0000

    semi-working final product

--- rmg.py
+++ rmg.py
@@ -1,5 +1,6 @@
 import requests
 import lxml.html
+import html2text
 
 login_url = 'https://intranet.hbtn.io/auth/sign_in'
 request_url = 'https://intranet.hbtn.io/projects/208'
@@ -14,5 +15,27 @@ form['user[login]'] = '320' # username
 form['user[password]'] = 'ar7AnbJn9sH#' #password
 response = s.post(login_url, data=form)
 
-page = s.get(request_url)
+page = s.get(request_url)       # get the url we really want
+page_html = lxml.html.fromstring(page.text) # turn it into lxml html
 
+# headers = page_html.xpath(r'//div[@class=" clearfix gap"]/h4[@class="task"]') # get all the header names
+# for elem in headers:
+#     print("######")
+#     print(lxml.html.tostring(elem, pretty_print=True))
+# headers = [x.strip(' \t\n\r') for x in headers] # remove spaces and extra lines
+# headers = filter(None, headers) # filter out blank entries    
+
+desc =  page_html.xpath(r'//div[@class=" clearfix gap"]/h4[@class="task"]|//div[@class=" clearfix gap"]/p|//div[@class=" clearfix gap"]/pre|//div[@class=" clearfix gap"]/ul') # get all the relevant values from the descriptions
+
+readme_html = open('README.html', 'w')
+
+for elem in desc:
+    readme_html.write(lxml.html.tostring(elem, pretty_print=True))
+
+readme_html.close()
+
+readme = open('README.md_g', 'w')
+
+readme.write(html2text.html2text('README.html'))
+readme.close()
+# print desc
