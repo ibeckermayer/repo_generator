281d6ae1b1f899b47cf271ee819e7cedddc8676e
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-14 22:25:59 +0000

    handle code

--- rmg.py
+++ rmg.py
@@ -26,16 +26,16 @@ page_html = lxml.html.fromstring(page.text) # turn it into lxml html
 # headers = filter(None, headers) # filter out blank entries    
 
 desc =  page_html.xpath(r'//div[@class=" clearfix gap"]/h4[@class="task"]|//div[@class=" clearfix gap"]/p|//div[@class=" clearfix gap"]/pre|//div[@class=" clearfix gap"]/ul') # get all the relevant values from the descriptions
-
 readme_html = open('README.html', 'w')
-
 for elem in desc:
     readme_html.write(lxml.html.tostring(elem, pretty_print=True))
-
 readme_html.close()
 
+readme_html = open('README.html', 'r').read()
 readme = open('README.md_g', 'w')
+text_maker = html2text.HTML2Text()
+text_maker.mark_code = True
 
-readme.write(html2text.html2text('README.html'))
+readme.write(text_maker.handle(readme_html).encode('utf-8'))
 readme.close()
 # print desc
