219c4fea0099ed95cfc1c71fb183220e8ea3def4
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-20 01:24:56 +0000

    appears to be working

--- repo_gen_utils.py
+++ repo_gen_utils.py
@@ -1,5 +1,6 @@
 import re
 import os
+import code
 
 
 def file_handle(f):
@@ -14,7 +15,6 @@ def file_handle(f):
         for directory in dirs_and_file[:-1]:
             try:
                 os.mkdir(directory.strip())
-                print(directory)
             except FileExistsError:
                 pass
 
@@ -23,3 +23,35 @@ def file_handle(f):
             os.mknod(fi.strip())
         except FileExistsError:
             pass
+
+
+def snippet_handle(s):
+    l = 0
+    lines = s.splitlines()
+    # code.interact(local=locals())
+    while l < len(lines):
+        words = re.split(r'[ ]', lines[l])
+        i = 0
+        while i < len(words):
+            if words[i] == "cat":
+                i+=1
+                l+=1
+                if i < len(words):
+                    while words[i][0] == "-":  # skip options
+                        i+=1
+                    filename = words[i]
+                    try:            # make the file
+                        os.mknod(filename.strip())
+                    except FileExistsError:
+                        pass
+                    while l < len(lines):  # continue down line by line
+                        if '@' not in lines[l] and '$' not in lines[l]:
+                            with open(filename, 'a') as f:
+                                f.write(lines[l] + '\n')
+                            l+=1
+                        else:
+                            break
+                    break
+            else:
+                i+=1
+        l+=1
